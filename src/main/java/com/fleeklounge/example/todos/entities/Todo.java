b816087b32ca8011e3add6ec9adf33c78a03cf0b
Asanobm <asanobm@outlook.com>
2024-01-19 19:44:08 +0900

    add: generate id

--- src/main/java/com/fleeklounge/example/todos/entities/Todo.java
+++ src/main/java/com/fleeklounge/example/todos/entities/Todo.java
@@ -1,15 +1,19 @@
 package com.fleeklounge.example.todos.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 @Data
 @Entity()
 @Table(name = "todos")
 public class Todo {
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private String title;
     private String description;
